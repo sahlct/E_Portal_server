8e6028f02f9a8b128e9ea5962411e80801e84697
RASI <74103061+rasi-c@users.noreply.github.com>
2025-12-20 17:56:25 +0530

    Update server.js for pulling from vps

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
-import seedAdminUser from "./src/seeders/admin.seeder.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const MONGO_URI = process.env.MONGO_URI;
 
 const startServer = async () => {
-  await connectDB(MONGO_URI);
-
-  // Seed admin user
-  await seedAdminUser();
-
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-  });
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Backend running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("❌ Server failed to start:", error.message);
+    process.exit(1);
+  }
 };
 
 startServer();
+
